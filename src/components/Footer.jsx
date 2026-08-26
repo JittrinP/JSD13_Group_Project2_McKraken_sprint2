fc4af28fdf9566ca71528e2679795d79dcf6bc10
AlbertCap <albert.phonbut@gmail.com>
2026-08-26 11:50:48 +0700

    setup folder and rounter

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <>
+      <div>
+        <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+          <div>
+            <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
+}
