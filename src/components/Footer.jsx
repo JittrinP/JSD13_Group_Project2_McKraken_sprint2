2148adb23fd7f37ab2da272648fdc190c1b28580
Jittrin <Jittrin.Puttawetmongkol@gmail.com>
2026-08-27 12:28:36 +0700

    footer :finish footer and add background color and add font to App.css in inline theme

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,38 @@ export default function Footer() {
   return (
     <>
       <div>
-        <footer className="footer footer-center p-10  text-base-content rounded bg-primary">
-          <div>
-            <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+        <footer className="w-full h-fit footer footer-center text-base-content rounded bg-background text-neutral">
+          {/* Display desktop */}
+
+          <div className="w-full h-fit flex flex-row justify-center font-body md:hidden">
+            <div>
+              <h2 className="font-display font-medium text-xl mb-4 mt-4">
+                Atelier de Flora
+              </h2>
+              <div className="flex flex-row gap-6 text-xs text-center justify-center mb-4 mt-4">
+                <p>Shipping</p>
+                <p>Contact Us</p>
+              </div>
+              <p className="text-base mb-4">© 2026 Atelier de Flora</p>
+            </div>
+          </div>
+
+          {/* Mobile desktop */}
+
+          <div className="hidden w-full justify-between font-body md:flex pl-16 pr-16">
+         
+              <div>
+                <h2 className="font-display font-medium text-xl mb-4 mt-4">
+                  Atelier de Flora
+                </h2>
+                <p className="text-base mb-4">© 2026 Atelier de Flora</p>
+              </div>
+
+              <div className="flex flex-row gap-6 text-xs text-center justify-center mb-4 mt-4">
+                <p>Shipping</p>
+                <p>Contact Us</p>
+              </div>
+   
           </div>
         </footer>
       </div>
