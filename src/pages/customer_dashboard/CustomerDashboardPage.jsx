32c9cb4ac32cd180a687d53f4ec07a97d30efdce
Jittrin <Jittrin.Puttawetmongkol@gmail.com>
2026-09-02 16:31:08 +0700

    page-arrangement : transfer from components  admin / customer dashboard/ homepage to pages

---
+++ src/pages/customer_dashboard/CustomerDashboardPage.jsx
@@ -0,0 +1,10 @@
+import CustomList from "../customer_dashboard/_components/CustomList"
+export default function CustomerDashboardPage() {
+  return (
+    <>
+      <div className="bg-amber-100">This is Customer Dashboard Page
+        <CustomList />
+      </div>
+    </>
+  );
+}
