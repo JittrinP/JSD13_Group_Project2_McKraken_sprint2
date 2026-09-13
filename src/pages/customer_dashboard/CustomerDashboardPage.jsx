4608ff8bf2ea5c06c36562a5be8f2397d1ca9352
Jittrin <Jittrin.Puttawetmongkol@gmail.com>
2026-09-13 10:54:07 +0700

    page-CustomerDashboardPage add mockup A-side customer for make layout right side components

--- src/pages/customer_dashboard/CustomerDashboardPage.jsx
+++ src/pages/customer_dashboard/CustomerDashboardPage.jsx
@@ -1,9 +1,21 @@
-import CustomList from "../customer_dashboard/_components/CustomList"
+import CustomList from "../customer_dashboard/_components/CustomList";
 export default function CustomerDashboardPage() {
   return (
     <>
-      <div className="bg-amber-100">This is Customer Dashboard Page
-        <CustomList />
+      <div className="bg-amber-100">
+        {/* Mock A-side คร่าวๆ แล้วไปทำ ด้านข้าง */}
+        <div className="flex flex-row gap-2">
+          {/* A-side desktop */}
+          <div className="hidden md:flex md:w-80 border-2 rounded-xl m-2 p-2">
+            A-side customer dashboard
+          </div>
+          {/* A-side mobile ค่อยว่ากัน น่าจะต้องสอดเข้าไปใน component แต่ละอันมั้ย */}
+     
+
+          {/* รอ AsideCustomer dashboard */}
+          {/* <AsideCustomer /> */}
+          <CustomList />
+        </div>
       </div>
     </>
   );
